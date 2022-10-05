7779f21ce0cda393f08cee4c7ef3d0109ad1a16e
NgoNhatQuang <quang.ngonhat98@gmail.com>
2022-10-05 15:22:00 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,46 @@
+import { Fragment } from "react";
+import Head from "next/head";
+import { MongoClient } from "mongodb";
+
+import MeetupList from "../components/meetups/MeetupList";
+
+const HomePage = (props) => {
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React Meetups"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </Fragment>
+  );
+};
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://meetupUser:meetupUser@cluster0.wmtrt.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetsup");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        image: meetup.image,
+        address: meetup.address,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 10,
+  };
+}
+
+export default HomePage;
